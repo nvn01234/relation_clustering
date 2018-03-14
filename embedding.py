592c40d7ad38997bcb91896945ee2bce623fdd67
nhatnv <nguyenvannhat152@gmail.com>
2018-03-15 00:01:45 +0900

    init

---
+++ embedding.py
@@ -0,0 +1,25 @@
+from gensim.models import KeyedVectors
+
+
+def main():
+    print("read word embeddings")
+    word2vec = KeyedVectors.load_word2vec_format("data/embeddings/wiki.vi.bin", binary=True).wv
+
+    print("read relations")
+
+    with open("data/RelationNormalize.txt", "r", encoding="utf8") as fin, \
+         open("data/relations_x.txt", "w", encoding="utf8") as fx, \
+         open("data/relations_y.txt", "w", encoding="utf8") as fy:
+        for line in fin:
+            w = line.strip().lower()
+            if w in word2vec:
+                fx.write(w)
+                fy.write(" ".join(list(word2vec.word_vec(w))) + "\n")
+            elif w.replace(" ", "_") in word2vec:
+                w = w.replace(" ", "_")
+                fx.write(w)
+                fy.write(" ".join(list(word2vec.word_vec(w))) + "\n")
+
+
+if __name__ == "__main__":
+    main()
