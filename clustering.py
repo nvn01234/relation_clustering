2c5eed5b9c70b3ff2849c954ff7adfdbc9c6d89b
nhatnv <nguyenvannhat152@gmail.com>
2018-03-19 15:38:39 +0700

    fix

--- clustering.py
+++ clustering.py
@@ -9,13 +9,17 @@ N_CLUSTERS = 50
 def calculate_distance_matrix():
     vectors = np.loadtxt("data/vectors.txt")
     distance_matrix = np.zeros([len(vectors), len(vectors)])
+    distances = [[]]
     for i in range(len(vectors) - 1):
         for j in range(i+1, len(vectors)):
             v1 = vectors[i]
             v2 = vectors[j]
-            distance_matrix[i, j] = distance_matrix[j, i] = distance.euclidean(v1, v2)
+            dis = distance.euclidean(v1, v2)
+            distance_matrix[i, j] = distance_matrix[j, i] = dis
+            distances.append(dis)
     np.save("data/distance_matrix.npy", distance_matrix)
-    print("max distance: %.4f, average distance: %.4f, min distance: %.4f" % (np.max(distance_matrix), np.average(distance_matrix), np.min(distance_matrix)))
+    np.save("data/distances.npy", distances)
+    print("max distance: %.4f, average distance: %.4f, min distance: %.4f" % (np.max(distances), np.average(distances), np.min(distances)))
 
 def main():
     vectors = np.loadtxt("data/vectors.txt")
